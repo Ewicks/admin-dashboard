201220624e061bf4cc66732fa5a00598d5fb3562
ethan <ethanowicks9@gmail.com>
2024-01-06 21:54:33 +0000

    Add browser router and tooltipcomponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { browserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import './App.css';
 
 const app = () => {
+  const activeMenu = true;
   return (
-    <h1 className='underline text-3xl' >app</h1>
+    <div>
+      <browserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+            <TooltipComponent content='settings' position='Top'>
+              <button type='button' className='text-3xl p-3 
+              hover:drop-shadow-xl hover:bg-light-gray text-white'
+              style={{ background: 'blue', borderRadius: '50%' }}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div>
+              Sidebar
+            </div>
+          ) : (
+            <div>
+              Sidebar w-0
+            </div>
+          )}
+
+        </div>
+      </browserRouter>
+    </div>
+
   )
 }
 
