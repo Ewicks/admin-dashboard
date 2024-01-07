1693b5c76767b2f8ac000c04f8399f810efda1ae
ethan <ethanowicks9@gmail.com>
2024-01-07 13:54:51 +0000

    Add routes and styling to divs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
 
 const app = () => {
-  const activeMenu = true;
+  const activeMenu = false;
   return (
     <div>
       <browserRouter>
@@ -21,14 +21,54 @@ const app = () => {
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div>
+            <div className='w-72 fixed sidebar
+            dark:bg-secondary-dark-bg
+            bg-white'>
               Sidebar
             </div>
           ) : (
-            <div>
-              Sidebar w-0
+            <div className='w-0 dark:bg-secondary-dark-bg'>
+              Sidebar
             </div>
           )}
+
+          <div className={
+            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+          }>
+            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+              navbar
+            </div>
+          </div>
+
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path='/' element='ECommerce'/>
+              <Route path='/ecommerce' element='ECommerce'/>
+
+              {/* Pages */}
+              <Route path='/orders' element='Orders'/>
+              <Route path='/employees' element='Employees'/>
+              <Route path='/customers' element='Customers'/>
+
+              {/* Apps */}
+              <Route path='/kanban' element='Kanban'/>
+              <Route path='/editor' element='Editor'/>
+              <Route path='/calender' element='Calender'/>
+              <Route path='/color-picker' element='ColorPicker'/>
+
+                {/* Charts */}
+                <Route path='/line' element='Line'/>
+                <Route path='/area' element='Area'/>
+                <Route path='/bar' element='bar'/>
+                <Route path='/pie' element='Pie'/>
+                <Route path='/financial' element='Financial'/>
+                <Route path='/pyramid' element='Pyramid'/>
+                <Route path='/stacked' element='Stacked'/>
+
+            </Routes>
+          </div>
+
 
         </div>
       </browserRouter>
