bb61bd563b09bf4cb562ea9c6e01648030aface4
ethan <ethanowicks9@gmail.com>
2024-01-07 16:33:15 +0000

    Add contexts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
@@ -7,10 +7,14 @@ import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Calendar, Orders, Employees, Stacked, Pyramid, Customers, 
   Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Line, Editor } from './pages';
 
+import { useStateContext } from './contexts/ContextProvider';
+
 import './App.css';
 
 const app = () => {
-  const activeMenu = true;
+
+  const { activeMenu } = useStateContext();
+
   return (
     <div>
       <BrowserRouter>
