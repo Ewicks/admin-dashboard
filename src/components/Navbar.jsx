d8238ee81fb63502d594d6ed8a588d2bc24a0505
ethan <ethanowicks9@gmail.com>
2024-01-09 19:10:37 +0000

    Add banner image and cards for ecommerce

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,9 +16,8 @@ const NavButton = ({title, customFunc, icon, color, dotColor}) => (
     <button type='button' onClick={customFunc} style={{ color }}
     className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
       <span style={{ background : dotColor }} className='absolute
-       inline-flex rounded-full h-2 w-2 right-2 top-2'>
-        {icon}
-      </span>
+       inline-flex rounded-full h-2 w-2 right-2 top-2' />
+      {icon}
     </button>
   </TooltipComponent>
 
@@ -39,7 +38,7 @@ const Navbar = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  
+
   // if width less than 900 hide sidebar
   useEffect(() => {
     if(screenSize <= 900) {
