89c1e34b8582533bd53c53f3ebf3c02467b8da39
ethan <ethanowicks9@gmail.com>
2024-01-09 21:38:56 +0000

    Add Sparkline chart with data

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BsCurrencyDollar } from 'react-icons/bs';
-import { GoPrimitiveDot } from 'react-icons/go';
+import { GoDot } from 'react-icons/go';
 import { IoIosMore } from 'react-icons/io';
 import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
 
@@ -48,6 +48,49 @@ const Ecommerce = () => {
 
         </div>
       
+      </div>
+      <div className='flex gap-10 flex-wrap justify-center'>
+          <div className='bg-white dark:text-gray-200
+           dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
+            <div className='flex justify-between'>
+              <p className='font-semibold text-xl'>
+                Revenue Updates
+              </p>
+              <div className='flex items-center gap-4'>
+                <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
+                  <span><GoDot /></span>
+                  <span>Expense</span>
+                </p>
+                <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'>
+                  <span><GoDot /></span>
+                  <span>Budget</span>
+                </p>
+              </div>
+            </div>
+            <div className='empty-10 flex gap-10 flex-wrap justify-center'>
+              <div className='border-r-1 border-color m-4 pr-10'>
+                <div>
+                  <p>
+                    <span className='text-3xl font-semibold'>£93,938</span>
+                    <span className='p-1.5 hover:drop-shadow-xl cursor-pointer
+                     rounded-full text-white bg-green-400 ml-3 text-xs'>23%</span>
+                  </p>
+                  <p className='mt-1 text-gray-500'>Budget</p>
+                </div>
+                <div className='mt-8'>
+                  <p>
+                    <span className='text-3xl font-semibold'>£200,938</span>
+                  </p>
+                  <p className='mt-1 text-gray-500'>Expense</p>
+                </div>
+                <div className='mt-5'>
+                  <SparkLine currentColor='blue' id='line-sparkline'
+                   type='Line' height='80px' width='250px' data={SparklineAreaData} color='blue' />
+                </div>
+              </div>
+
+            </div>
+           </div>
       </div>
     </div>
   )
