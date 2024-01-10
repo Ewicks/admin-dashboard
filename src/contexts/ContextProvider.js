513f633163683363d9905215713eb8c0dc6b7681
ethan <ethanowicks9@gmail.com>
2024-01-10 18:50:02 +0000

    Add settings side panel and open and close functionality

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -14,6 +14,21 @@ export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
     const [isClicked, setisClicked] = useState(initialState)
     const [screenSize, setScreenSize] = useState(undefined)
+    const [currentColor, setCurrentColor] = useState('#03c9d7')
+    const [currentMode, setCurrentMode] = useState('Light')
+    const [themeSettings, setThemeSettings] = useState(false)
+
+    const setMode = (e) => {
+        setCurrentMode(e.target.value);
+
+        localStorage.setItem('themeMode', e.target.value)
+    } 
+
+    const setColor = (e) => {
+        setCurrentColor(e.target.value);
+
+        localStorage.setItem('colorMode', e.target.value)
+    } 
 
     const handleClick = (clicked) => {
         setisClicked({ ...initialState, [clicked]: true});
@@ -21,7 +36,10 @@ export const ContextProvider = ({ children }) => {
 
 
     return (
-        <StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, setisClicked, handleClick, screenSize, setScreenSize }}>
+        <StateContext.Provider value={{ activeMenu, setActiveMenu,
+         isClicked, setisClicked, handleClick, screenSize, setScreenSize,
+         currentColor, currentMode, setCurrentColor, setCurrentMode,
+         themeSettings, setThemeSettings }}>
             {children}
         </StateContext.Provider>
     )
